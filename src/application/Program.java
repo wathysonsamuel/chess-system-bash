745e935d3cedd789132e0375805509be20fa6d8e
Wathyson Samuel <wathysonsamuel61@gmail.com>
2025-07-09 14:48:14 -0300

    create class position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,10 @@
 package application;
 
+import application.boardgame.Position;
+
 public class Program {
+    public static void main(String[] args) {
+        Position pos = new Position(5,3);
+        System.out.println(pos);
+    }
 }
